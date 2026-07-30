d2bbe6b323a6cfaa98bb6791730a1a3fbdd4b953
gina chilambo <ginachilambo@Genesis.local>
2026-07-30 01:51:42 -0500

    fix: use explode() in cors.php to properly handle multiple allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
